337b21f3ab994b0bbc6432260526729390c0fce2
NEKOTENI <riverfield.tk@gmail.com>
2015-05-01 17:59:28 +0900

    make tickets application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tickets::Application.config.secret_key_base = '7d597497edc5d03949d63d3f9dd53f34ef1bd3b073baf052c526d1ff01d18ff287df95a62901e62b3dd9ed213beafab2dc049720951281b13befa2882e501758'
